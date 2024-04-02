4353bb6e69883394e66ee3d6881ffa798c98a76e
Siddharth Bhat <siddu.druid@gmail.com>
2024-04-02 13:55:01 +0100

    feat: start trying to run benching directly on CI so I am not on the critical path

--- 1-runs/1-SKELETON/ci-worker.sh
+++ 1-runs/1-SKELETON/ci-worker.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
+sed -i "s/COMMIT_TO_BENCH=.*/COMMIT_TO_BENCH=$(git rev-parse HEAD)/g" $EXPERIMENTDIR/speedcenter-worker.sh
 source speedcenter-worker.sh
+
+
+mkdir -p $EXPERIMENTDIR/builds-speedcenter
+for i in {0..1}; do
+    run_build_for_kind "${KINDS[i]}"
+done;
+
+for i in {0..1}; do
+    run_benchmark_for_kind "${KINDS[i]}"
+done;
+
+for i in {0..1}; do
+  cp "$EXPERIMENTDIR/speedcenter.exec.velcom.yaml" "$EXPERIMENTDIR/builds-speedcenter/${KINDS[i]}/tests/bench/speedcenter.exec.velcom.yaml"
+  run_temci_for_kind "${KINDS[i]}"
+done;
 
