e0523f90ada9e6fa726487592d25179c941c0411
Anton Lorenzen <anfelor@posteo.de>
2024-04-03 11:08:32 +0100

    chore: update stage0

--- stage0/src/include/lean/lean.h
+++ stage0/src/include/lean/lean.h
@@ -378,6 +378,7 @@ static inline void lean_free_small_object(lean_object * o) {
 }
 
 LEAN_EXPORT lean_object * lean_alloc_object(size_t sz);
+LEAN_EXPORT void lean_free_token(lean_object * o);
 LEAN_EXPORT void lean_free_object(lean_object * o);
 
 static inline uint8_t lean_ptr_tag(lean_object * o) {
@@ -482,6 +483,10 @@ static inline bool lean_is_exclusive(lean_object * o) {
     } else {
         return false;
     }
+}
+
+static inline bool lean_is_null(lean_object * a1) {
+    return lean_is_scalar(a1);
 }
 
 static inline bool lean_is_shared(lean_object * o) {
