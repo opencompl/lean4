44127be7ee6c60a158a156c5d80e11f508645c0e
Siddharth Bhat <siddu.druid@gmail.com>
2024-07-04 00:26:21 +0100

    chore: add one impl of div_invariant

--- src/Init/Data/BitVec/div_invariant.py
+++ src/Init/Data/BitVec/div_invariant.py
@@ -5,28 +5,26 @@ def print_bits(w, n):
     return ("{0:0%sb}" % (w)).format(n)
 
 def check_pre_invariant(w, n, d, q, r, j):
-    assert r < d
     qright = n // d
     rright = n % d
+    assert r < d
 
-    assert n >> (j + 1) == d * q + r
-    assert q == qright >> (j + 1)
-    assert r == ((n >> j) - (d * (qright >> j)))
-    pass
-
+# n / d <-> n = q * d + r
 def check_post_invariant(w, n, d, q, r, j):
     qright = n // d
     rright = n % d
     assert r < d
-    assert q == qright >> j
-    assert n >> j == d * q + r
-    assert r == ((n >> j) - (d * (qright >> j)))
+    nhigh = n >> j
+    print("   n >> j = %s | q(%s) * d(%s) + r(%s) = (%s)" % 
+            (print_bits(w, nhigh), print_bits(w, q), print_bits(w, d), print_bits(w, r), print_bits(w, q * d + r)))
+    assert nhigh == d * q + r
 
 def shift_subtract(w, n, d, q, r, j):
-    check_pre_invariant(w, n, d, q, r, j)
     print(f"shift_subtract> n: '%s' | d: '%s' | q : '%s' | r : '%s' | j : '%s'" % 
           (print_bits(w, n), print_bits(w, d), print_bits(w, q), print_bits(w, r), j))
-    print(f"  j[%s] = %s" % (j, get_lsb(n, j)))
+    print(f"  n[%s] = %s" % (j, get_lsb(n, j)))
+    check_pre_invariant(w, n, d, q, r, j)
+
     r = (r << 1) | get_lsb(n, j)
     print(f"  r = %s" % print_bits(w, r))
     if r >= d:
@@ -40,22 +38,24 @@ def shift_subtract(w, n, d, q, r, j):
         q = (q << 1)
         print(f"  r.new = %s" % print_bits(w, r))
         print(f"  q = %s" % print_bits(w, q))
+    check_post_invariant(w, n, d, q, r, j)
     if j == 0:
         (qout, rout) = (q, r)
     else:
         (qout, rout) = shift_subtract(w, n, d, q, r, j-1)
-    check_post_invariant(w, n, d, q, r, j)
     return (qout, rout)
 
 # 10 / 3 = 3
-for n in range(1, 10):
-    for d in range(1, 10):
-        w = 4
+for n in range(1, 32):
+    for d in range(1, 32):
+        w = 6
         (q, r) = shift_subtract(w, n, d, 0, 0, w-1)
         assert n == d * q + r
-        if n == d * q + r:
+        if n == d * q + r and r < d:
             print ("verified correct invariant for n: '%s' | d : '%s' | q : '%s' r: '%s'" % 
                    (n, d, q, r))
         else:
             raise RuntimeError("verification failed for n: '%s' | d: '%s'" % (n, d))
+
+
 
