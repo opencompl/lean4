a2c357603219185ffd9cbc26f121c8ec036f3d4d
Siddharth Bhat <siddu.druid@gmail.com>
2024-07-04 09:00:36 +0100

    chore: add new division invariant that should be easier to prove

--- src/Init/Data/BitVec/div_new_invariant.py
+++ src/Init/Data/BitVec/div_new_invariant.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 def get_lsb(n, j):
     return int(bool(n & (1 << j)))
 
@@ -10,22 +12,32 @@ def check_pre_invariant(w, n, d, q, r, j):
     assert r < d
 
 # n / d <-> n = q * d + r
-def check_post_invariant(w, n, d, q, r, j):
+def check_post_rec_invariant(w, n, d, q, r, j):
     qright = n // d
     rright = n % d
     assert r < d
-    nhigh = n >> j
-    print("   n >> j = %s | q(%s) * d(%s) + r(%s) = (%s)" % 
-            (print_bits(w, nhigh), print_bits(w, q), print_bits(w, d), print_bits(w, r), print_bits(w, q * d + r)))
-    assert nhigh == d * q + r
+    assert n >> (w - j) == d * q + r
+
+# n / d <-> n = q * d + r
+def check_final_invariant(w, n, d, q, r, j):
+    qright = n // d
+    rright = n % d
+    assert r < d
+    assert n >> ((w - 1) - j) == d * q + r
 
 def shift_subtract(w, n, d, q, r, j):
     print(f"shift_subtract> n: '%s' | d: '%s' | q : '%s' | r : '%s' | j : '%s'" % 
           (print_bits(w, n), print_bits(w, d), print_bits(w, q), print_bits(w, r), j))
     print(f"  n[%s] = %s" % (j, get_lsb(n, j)))
     check_pre_invariant(w, n, d, q, r, j)
+    if j > 0:
+        (q, r) = shift_subtract(w, n, d, q, r, j-1)
+        check_post_rec_invariant(w, n, d, q, r, j)
 
-    r = (r << 1) | get_lsb(n, j)
+    # do the last bit.
+    ix = (w - 1) - j
+    assert ix >= 0
+    r = (r << 1) | get_lsb(n, ix)
     print(f"  r = %s" % print_bits(w, r))
     if r >= d:
         print(f"  r > d.")
@@ -38,12 +50,8 @@ def shift_subtract(w, n, d, q, r, j):
         q = (q << 1)
         print(f"  r.new = %s" % print_bits(w, r))
         print(f"  q = %s" % print_bits(w, q))
-    check_post_invariant(w, n, d, q, r, j)
-    if j == 0:
-        (qout, rout) = (q, r)
-    else:
-        (qout, rout) = shift_subtract(w, n, d, q, r, j-1)
-    return (qout, rout)
+    check_final_invariant(w, n, d, q, r, j)
+    return (q, r)
 
 # 10 / 3 = 3
 for n in range(1, 32):
