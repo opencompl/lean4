2ed5e29b1d222430b2a3172c06adb970b278d921
Siddharth Bhat <siddu.druid@gmail.com>
2024-10-08 22:55:22 -0500

    chore: add prover shootout example

--- tests/lean/run/ackermannize.lean
+++ tests/lean/run/ackermannize.lean
@@ -24,3 +24,20 @@ theorem bar (f : BitVec 1 -> BitVec 1) (x y : BitVec 1)
  
 /-- info: 'bar' depends on axioms: [propext, Classical.choice, Lean.ofReduceBool, Quot.sound] -/
 #guard_msgs in  #print axioms bar
+
+-- set_option trace.ack true in
+theorem less_6E (m : BitVec 64) (P : BitVec 64 → Bool)
+    (h : m ≤ 0x00000006 ∧
+         P 0x00000000 ∧
+         P 0x00000001 ∧
+         P 0x00000002 ∧
+         P 0x00000003 ∧
+         P 0x00000004 ∧
+         P 0x00000005 ∧
+         P 0x00000006) :
+    P m := by
+  bv_ack_eager
+  bv_decide
+
+/-- info: 'less_6E' depends on axioms: [propext, Classical.choice, Lean.ofReduceBool, Quot.sound] -/
+#guard_msgs in  #print axioms less_6E
