0996840aec5c92722a43d1fa4a32d40f7384112e
Alex Keizer <alex@keizer.dev>
2025-01-22 17:50:59 +0000

    test cases

--- src/Lean/Meta/Tactic/AC/Sharing.lean
+++ src/Lean/Meta/Tactic/AC/Sharing.lean
@@ -270,3 +270,34 @@ elab "ac_nf!" loc?:(location)? : tactic => do
     | Location.wildcard =>
       acNfTargetTactic
       (← (← getMainGoal).getNondepPropHyps).forM acNfHypTactic
+
+/-! ## Tests -/
+
+section Tests
+
+-- TODO: these tests should be moved to their own file
+
+theorem mul_mul_eq_mul_mul (x₁ x₂ y₁ y₂ z : BitVec 4) (h₁ : x₁ = x₂) (h₂ : y₁ = y₂) :
+    x₁ * (y₁ * z) = x₂ * (y₂ * z) := by
+  ac_nf!
+  show z * (x₁ * y₁) = z * (x₂ * y₂)
+  sorry
+
+-- This theorem is short-circuited and scales to standard bitwidths.
+theorem mul_eq_mul_eq_right (x y z : BitVec 64) (h : x = y) :
+    x * z = y * z := by
+  ac_nf!
+  show z * x = z * y
+  sorry
+
+-- This theorem is short-circuited and scales to standard bitwidths.
+theorem mul_eq_mul_eq_left (x y z : BitVec 64) (h : x = y) :
+    z * x = z * y := by
+  ac_nf!
+  show z * x = z * y
+  sorry
+
+theorem short_circuit_triple_mul (x x_1 x_2 : BitVec 32) (h : ¬x_2 &&& 4096#32 == 0#32) :
+    (x_1 ||| 4096#32) * x * (x_1 ||| 4096#32) = (x_1 ||| x_2 &&& 4096#32) * x * (x_1 ||| 4096#32) := by
+  ac_nf!
+  sorry
