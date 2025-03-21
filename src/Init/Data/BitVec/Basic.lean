6628255dfb29303448c4ffcadea0e203ca3bbf75
luisacicolini <luisacicolini@gmail.com>
2025-03-21 11:45:34 +0000

    chore: fix def in basic

--- src/Init/Data/BitVec/Basic.lean
+++ src/Init/Data/BitVec/Basic.lean
@@ -702,7 +702,7 @@ def negOverflow {w : Nat} (x : BitVec w) : Bool :=
 negOverflow x` returns `true` if the divisioin of `x` by `y` results in overflow.
 For BitVecs `x` and `y` with width `0 < w`, this only happens if `x = intMin` and `y = allOnes w`.
 
-  SMT-LIB name: `bvnego`.
+  SMT-LIB name: `bvsdivo`.
 -/
 def sdivOverflow {w : Nat} (x y : BitVec w) : Bool :=
   (x.toInt / y.toInt ≥ 2 ^ (w - 1)) || (x.toInt / y.toInt < - 2 ^ (w - 1))
