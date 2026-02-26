db7cbb2bad1896abd44127a532478a36ce8b49ba
luisacicolini <lc985@cam.ac.uk>
2026-02-26 13:12:51 +0000

    chore: test

--- tests/elab/bv_decide_rewriter.lean
+++ tests/elab/bv_decide_rewriter.lean
@@ -684,6 +684,7 @@ example : (BitVec.allOnes 3).cpop = 3#3 := by bv_decide
 example : (0#64).cpop = 0#64 := by bv_decide
 example {x : BitVec 8} : x.cpop = (x.reverse).cpop := by bv_decide
 example {x y : BitVec 8} : (x ++ y).cpop = (x.zeroExtend 16).cpop + (y.zeroExtend 16).cpop:= by bv_decide
+example {x y : BitVec 8} : (x ++ y).cpop = (x.cpop + y.cpop).zeroExtend 16 := by bv_decide
 
 section
 
